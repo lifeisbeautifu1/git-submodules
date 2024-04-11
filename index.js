ba527de6f6e1218c457bfa256271bcc67fdd7e49
lifeisbeautiful <poltoradnevone@gmail.com>
2024-04-11 10:13:28 +0400

    feat: updated submodule

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import data from "./submodule/data.json" with { type: "json" };
-import { sum } from "./submodule/utils/index.js";
+import { sum, minus } from "./submodule/utils/index.js";
 
 console.log("Hello world!");
 
 console.log(`Here's data from the submodule`, data);
 
 console.log(`5 + 3 = ${sum(5, 3)}`);
+
+console.log(`5 - 3 = ${minus(5, 3)}`);
