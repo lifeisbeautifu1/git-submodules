eb0faa69484713f8462057113638b06fb9d7c0ba
lifeisbeautiful <poltoradnevone@gmail.com>
2024-04-11 10:02:37 +0400

    feat: using utils from submodule

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+import data from "./submodule/data.json" with { type: "json" };
+import { sum } from "./submodule/utils/index.js";
+
 console.log("Hello world!");
+
+console.log(`Here's data from the submodule`, data);
+
+console.log(`5 + 3 = ${sum(5, 3)}`);
